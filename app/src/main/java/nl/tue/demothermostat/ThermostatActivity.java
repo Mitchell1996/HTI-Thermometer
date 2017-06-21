c64157fe7b244244d31b0520bfc7bbb418474175
Mitchell1996 <Mitchellpijl1996@hotmail.com>
2017-06-21 21:07:09 +0200

    ThermostatActivity layout
    Interface proberen te veranderen voor de app. Weet nog niet hoe ik van
    dacht naar nacht ga.

--- app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
+++ app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class ThermostatActivity extends Activity {
@@ -14,6 +16,7 @@ public class ThermostatActivity extends Activity {
     int vtemp = 21;
     TextView temp;
     SeekBar seekBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +46,15 @@ public class ThermostatActivity extends Activity {
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), TestingWS.class);
                 startActivity(intent);
+            }
+        });
+
+        Switch dNSwitch = (Switch)findViewById(R.id.switch1);
+
+        dNSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
             }
         });
 
