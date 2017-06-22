ef5b2ad9145636983020ecebf64ca048028ac2c9
Mitchell1996 <Mitchellpijl1996@hotmail.com>
2017-06-23 00:10:48 +0200

    Week Overview Layout
    Changed the Layout for week Overview

--- app/src/main/java/nl/tue/demothermostat/WeekOverview.java
+++ app/src/main/java/nl/tue/demothermostat/WeekOverview.java
@@ -3,6 +3,8 @@ package nl.tue.demothermostat;
 import android.app.Activity;
 import android.os.Bundle;
 
+import org.thermostatapp.util.WeekProgram;
+
 /**
  * Created by nstash on 06/05/15.
  */
@@ -11,5 +13,8 @@ public class WeekOverview extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.week_overview);
+
+        WeekProgram weekProgram =  new WeekProgram();
+        weekProgram.setDefault();
     }
 }
