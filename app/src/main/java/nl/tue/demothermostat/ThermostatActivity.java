60668f20d4afabce7dc32cf72a4c90232a3626d4
Noodlegg <TGian98@gmail.com>
2017-06-23 06:39:47 +0200

    Current Temperature realtime unfixed
    Latest progress made, still can't make UI show actual current temperature from server.

--- app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
+++ app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
@@ -2,13 +2,13 @@ package nl.tue.demothermostat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -16,37 +16,23 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import org.thermostatapp.util.HeatingSystem;
-
 import java.net.ConnectException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ThermostatActivity extends Activity {
 
     public static double temp, temp_current, temp_target, temp_day, temp_night;             //this should be retrieved from the server to show the current temp
     TextView currentTemp, targetTemp;
+    private Handler yourHandler = new Handler();
+    private Timer yourTimer = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thermostat);
         final LinearLayout ll = (LinearLayout)findViewById(R.id.Parent);
-
-        //Gets day/night temperatures from server and stores them in the variables
-        /*
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    temp_day = Double.parseDouble(HeatingSystem.get("dayTemperature"));
-                    temp_night = Double.parseDouble(HeatingSystem.get("nightTemperature"));
-                    temp_current = Double.parseDouble(HeatingSystem.get("currentTemperature"));
-                    temp_target = Double.parseDouble(HeatingSystem.get("targetTemperature"));
-                    currentTemp.setText(String.valueOf(temp_current));
-                    targetTemp.setText(String.valueOf(temp_target));
-                } catch (ConnectException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start(); */
 
         currentTemp = (TextView)findViewById(R.id.currentTemp);
         targetTemp = (TextView)findViewById(R.id.targetTemp);
@@ -55,14 +41,30 @@ public class ThermostatActivity extends Activity {
         Button weekOverview = (Button)findViewById(R.id.week_overview);
         final ToggleButton targetToggle = (ToggleButton) findViewById(R.id.targetToggle);
 
-        /*
-        runOnUiThread(new Runnable() {
+        //Gets day/night temperatures from server and stores them in the variables
+        AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                currentTemp.setText(String.valueOf(temp_current));
+                try {
+                    temp_day = Double.parseDouble(HeatingSystem.get("dayTemperature"));
+                    temp_night = Double.parseDouble(HeatingSystem.get("nightTemperature"));
+                    temp_current = Double.parseDouble(HeatingSystem.get("currentTemperature"));
+                    temp_target = Double.parseDouble(HeatingSystem.get("targetTemperature"));
+                } catch (ConnectException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+        if (yourTimer != null) {
+            yourTimer.cancel();
+        }
+        yourTimer = new Timer();
+        yourTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 0, 1000);
+
+
+
+        /*
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -198,4 +200,17 @@ public class ThermostatActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+    class TimeDisplayTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            // run on another thread
+            yourHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    currentTemp.setText(String.valueOf(temp_current));
+                }
+            });
+        }
+    }
 }
+
