26fca0339cefef65b4afc90a5aff5b887399a21b
Mitchell1996 <Mitchellpijl1996@hotmail.com>
2017-06-23 22:25:30 +0200

    Navigationbar

--- app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
+++ app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -213,6 +216,31 @@ public class ThermostatActivity extends Activity {
                 }).start(); */
             }
         });
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.Thermostat:
+
+                                break;
+                            case R.id.WeekOverview:
+                                Intent intent2 = new Intent(getBaseContext(), WeekOverview.class);
+                                startActivity(intent2);
+                                break;
+                            case R.id.Test:
+                                Intent intent3 = new Intent(getBaseContext(), WeekOverview.class);
+                                startActivity(intent3);
+                                break;
+                        }
+                        return false;
+                    }
+                });
 
     }
 
