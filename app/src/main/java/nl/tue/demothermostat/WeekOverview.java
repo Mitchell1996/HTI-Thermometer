19064cb2fe848943e1ecb0d8915227eb6ccc4189
Mitchell1996 <Mitchellpijl1996@hotmail.com>
2017-06-23 21:57:56 +0200

    New them and bottom navigation bar
    Does not work yet though

--- app/src/main/java/nl/tue/demothermostat/WeekOverview.java
+++ app/src/main/java/nl/tue/demothermostat/WeekOverview.java
@@ -2,19 +2,35 @@ package nl.tue.demothermostat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import org.thermostatapp.util.WeekProgram;
 
 /**
  * Created by nstash on 06/05/15.
  */
-public class WeekOverview extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.week_overview);
 
-        WeekProgram weekProgram =  new WeekProgram();
-        weekProgram.setDefault();
+        Button monday = (Button)findViewById(R.id.Monday);
+        Button tuesday = (Button)findViewById(R.id.Tuesday);
+        Button wednesday = (Button)findViewById(R.id.Wednesday);
+        Button thursday = (Button)findViewById(R.id.Thursday);
+        Button friday = (Button)findViewById(R.id.Friday);
+        Button saturday = (Button)findViewById(R.id.Saturday);
+        Button sunday = (Button)findViewById(R.id.Sunday);
+
+        monday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+
+            }
+        });
+
+
     }
 }
